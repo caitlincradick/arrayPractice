e9d8acc39b32378f4649907be16528f955404df4
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-07 11:10:57 -0800

    add lines 14- 23

--- arrays.js
+++ arrays.js
@@ -1,12 +1,23 @@
-var pets = ["cat", "snake", "tarantula", "bearded dragon"];
-var cardNumber = [23,42,7,99];
-var paidTicket = [true, false, true, false];
+// var pets = ["cat", "snake", "tarantula", "bearded dragon"];
+// var cardNumber = [23,42,7,99];
+// var paidTicket = [true, false, true, false];
 
-pets.pop ();
-console.log(pets);
-//expected output: Array ["cat", "snake", "tarantula"]
-cardNumber.shift();
-console.log (cardNumber);
-//expected output: Array [42,7,99]
-console.log(paidTicket.slice(1));
-//expected output: Array [false, true, false]
+// pets.pop ();
+// console.log(pets);
+// //expected output: Array ["cat", "snake", "tarantula"]
+// cardNumber.shift();
+// console.log (cardNumber);
+// //expected output: Array [42,7,99]
+// console.log(paidTicket.slice(1));
+// //expected output: Array [false, true, false]
+
+// Index positions are the number(s) associated wiht elements from an arrya, bracket notoation and a number. It corresponds with that element that should be used. The counting starts with '0' because it takes 0 counts to get to the first element. i.e [87,9,76,4], 87 would be considered 0.
+var pets = ["cat", "snake", "tarantula", "bearded dragon"];
+// There are 4 elements in this Array
+// The index position of "cat" is 0
+// The value of the element in index position 2 is "tarantula"
+//The index position of the first element in this array is 0 and the position of the last element is 3
+console.log (pets[0]);
+//expected output: element ["cat"]
+console.log (pets[3]);
+//expected output: element ["bearded dragon"]
