50a34bc4c58add9cccc793e0a3ed7495303e1408
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-07 10:54:51 -0800

    add different array methods for each of the arrays created

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var pets = ["cat", "snake", "tarantula", "bearded dragon"];
 var cardNumber = [23,42,7,99];
-var paidTicket = [true, false, yes, no];
+var paidTicket = [true, false, true, false];
+
+pets.pop ();
+console.log(pets);
+//expected output: Array ["cat", "snake", "tarantula"]
+cardNumber.shift();
+console.log (cardNumber);
+//expected output: Array [42,7,99]
+console.log(paidTicket.slice(1));
+//expected output: Array [false, true, false]
